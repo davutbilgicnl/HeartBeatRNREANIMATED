f3a4c606e03fefe198957d4d10bd12327de1b208
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-10-11 12:44:30 +0200

    Revert "Refactor App.tsx and add HeartBeat component"
    This reverts commit 497be694a13fa4ce19590d9a3d80f9f12f8b881a.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import HeartBeat from './components/HeartBeat';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <HeartBeat />
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
